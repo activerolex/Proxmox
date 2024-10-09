97934c41072ae617ea32886dab6aac26619d873f
Javiere Gordon <92408106+activerolex@users.noreply.github.com>
2024-10-09 11:26:39 -0300

    Update nextcloud-vm.sh

--- vm/nextcloud-vm.sh
+++ vm/nextcloud-vm.sh
@@ -403,7 +403,7 @@ qm set $VMID \
   -efidisk0 ${DISK0_REF}${FORMAT} \
   -scsi0 ${DISK1_REF},${DISK_CACHE}${THIN}size=12G \
   -boot order=scsi0 \
-  -description "<div align='center'><a href='https://Helper-Scripts.com'><img src='https://raw.githubusercontent.com/activerolex/Proxmox/main/misc/images/logo-81x112.png'/></a>
+  -description "<div align='center'><a href='https://github.com'><img src='https://raw.githubusercontent.com/activerolex/Proxmox/main/misc/images/logo-81x112.png'/></a>
 
   # $NAME
 
